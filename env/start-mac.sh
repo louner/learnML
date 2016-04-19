a61a18d3639fa741e2908c835bc465fd260a0a83
Allen Kuo (RD-TW) <allen_kuo@tw-allenkuo2.client.tw.trendnet.org>
2016-04-19 15:09:28 +0800

    add comments

--- env/start-mac.sh
+++ env/start-mac.sh
@@ -1,6 +1,18 @@
 #! /bin/sh
 
+#docker-machine is installed
+
+#build docker vm
 docker-machine create --driver virtualbox default
+
+#start docker vm
 docker-machine start default
+
+#switch to docker vm
 eval "$(docker-machine env default)"
+
+#build docker image for happycat env
 docker build -t=happycat:v1 ./dockerfile/
+
+#switch into happycat vm
+docker run -it happycat:v1
